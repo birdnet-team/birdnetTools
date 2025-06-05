8aa155b338d9362b9bce18d32f4d6989fc62217b
Sunny Tseng <sunnyyctseng@gmail.com>
2025-06-05 14:25:26 +0200

    established the shiny app save function!

--- R/birdnet_launch_validation.R
+++ R/birdnet_launch_validation.R
@@ -15,8 +15,8 @@ birdnet_validation_ui <- function(request) {
     sidebar = bslib::sidebar(
       bslib::card("File import",
 
-                  # input: detection_list
-                  shiny::fileInput(inputId = "detection_list",
+                  # input: import_file
+                  shiny::fileInput(inputId = "import_file",
                                    label = "Metadata csv file",
                                    multiple = FALSE,
                                    accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv"),
@@ -45,6 +45,9 @@ birdnet_validation_ui <- function(request) {
                                       value = 512)
       ), bslib::card("Actions",
 
+                  # button: save_file
+                  shiny::downloadButton("save_file","Save"),
+
                   # button: praise_me
                   shiny::actionButton("praise_me", "Praise me")
       )
@@ -59,7 +62,9 @@ birdnet_validation_ui <- function(request) {
 
     # output: spectrogram
     bslib::card(
-      shiny::plotOutput("spectrogram")
+      shiny::verbatimTextOutput("test")
+      #DT::DTOutput("main_table_1")
+      #shiny::plotOutput("spectrogram")
       )
   )
 }
@@ -138,17 +143,18 @@ birdnet_validation_server <- function(input, output, session) {
 
   rv <- shiny::reactiveValues()
 
-  shiny::observeEvent(input$detection_list, {
+  # if importing file, create data_editable and data_display reactive values
+  shiny::observeEvent(input$import_file, {
 
     # read in the csv file and clean names based on the path input
     tryCatch({
-      rv$data_display <- input$detection_list$datapath |>
+      rv$data_editable <- input$import_file$datapath |>
         readr::read_csv(show_col_types = FALSE) |>
         birdnet_clean_names()
 
       # check if the data frame has the validation column already
-      if (!"validation" %in% names(rv$data_display)) {
-        rv$data_display <- rv$data_display |>
+      if (!"validation" %in% names(rv$editable)) {
+        rv$data_editable <- rv$data_editable |>
           dplyr::mutate(validation = "U")
       }
 
@@ -157,6 +163,16 @@ birdnet_validation_server <- function(input, output, session) {
         paste("Error reading CSV:", e$message),
         type = "error")
     })
+
+
+    # render the data table with a play button
+    rv$data_display <- rv$data_editable |>
+      dplyr::select("filepath", "scientific_name", "common_name",
+                    "start", "end", "validation") |>
+
+      dplyr::mutate(Spectrogram = '<button class="spectrogram">Spectrogram</button>',
+                    Audio = '<button class="play-audio">Audio</button>')
+
   })
 
 
@@ -167,57 +183,48 @@ birdnet_validation_server <- function(input, output, session) {
     # ensure the file has been uploaded before trying to render
     req(rv$data_display)
 
-
-    # render the data table with a play button
-    rv$data_display |>
-      dplyr::select("filepath", "scientific_name", "common_name", "start", "end", "validation") |>
-
-      dplyr::mutate(Spectrogram = '<button class="spectrogram">Spectrogram</button>',
-                    Audio = '<button class="play-audio">Audio</button>') |>
-
-    # dat$Decision <- sapply(seq_len(nrow(dat)), function(i) {
-    #   val <- dat$validation[i]
-    #   color <- switch(val, "Y" = "success", "N" = "danger", "U" = "primary")
-    #   label <- switch(val, "Y" = "✔️", "N" = "❌", "U" = "❔")
-    #   paste0('<button class="btn btn-', color,
-    #          ' btn-sm toggle-val" data-row="', i, '">', label, '</button>')
-    # })
-
-
-    DT::datatable(editable = list(target = "column", disable = list(columns = c(1, 2, 3, 4, 5))),
-                  escape = FALSE, # to render HTML properly
-                  selection = "none",
-                  options = list(
-                    pageLength = 10,
-                    columnDefs = list(
-                      list(targets = 7, orderable = FALSE),
-                      list(targets = 8, orderable = FALSE)
+      DT::datatable(rv$data_display ,
+                    editable = TRUE,
+                      #list(target = "column", disable = list(columns = c(1, 2, 3, 4, 5))),
+                    escape = FALSE, # to render HTML properly
+                    selection = "none",
+                    options = list(
+                      pageLength = 10,
+                      columnDefs = list(
+                        list(targets = 7, orderable = FALSE),
+                        list(targets = 8, orderable = FALSE)
+                      )
                     )
-                  )
-    #               ,
-    #               callback = JS("
-    #   table.on('click', '.toggle-val', function() {
-    #     var row = $(this).data('row');
-    #     Shiny.setInputValue('toggle_val', row, {priority: 'event'});
-    #   });
-    # ")
     )
   }, server = FALSE)
 
 
-
-  # when the value is edited
+  # when the table is clicked to change values
   observeEvent(input$main_table_cell_edit, {
 
-    info <- input$data_display_cell_edit
+    # get the edit information
+    info <- input$main_table_cell_edit
 
     # update the value in the reactive dataframe
-    rv$data_display[info$row, info$col] <- info$value
+    rv$data_editable$validation[info$row] <- info$value
+
+    output$test <- shiny::renderPrint(
+      "click triggered"
+    )
 
   })
 
 
   # Button observers -----------------------------------------------------
+
+  output$save_file <- downloadHandler(
+    filename = function() {
+      paste0("validated_", input$import_file, ".csv")
+    },
+    content = function(file) {
+      readr::write_csv(rv$data_editable, file)
+    }
+  )
 
   shiny::observeEvent(input$praise_me, {
     shiny::showNotification(praise::praise(), type = "message")
