758c1392da625a2748a41f2fb9fedf50ad0f7501
Sunny Tseng <sunnyyctseng@gmail.com>
2025-06-11 10:13:48 +0200

    Added initial test for the shiny server

--- tests/testthat/test-birdnet_launch_validation.R
+++ tests/testthat/test-birdnet_launch_validation.R
@@ -1,3 +1,24 @@
 test_that("shinyapp can launch after calling", {
   expect_equal()
 })
+
+
+test_that("Initial test for the server", {
+
+  testServer(birdnet_validation_server, {
+    session$setInputs(import_file = list(
+      datapath = testthat::test_path("data", "test_example_1.csv"),
+      name = "test_example_1.csv",
+      size = file.info(testthat::test_path("data", "test_example_1.csv"))$size,
+      type = "text/csv"
+    ))
+
+    # Trigger the observer and allow it time to run
+    session$flushReact()
+
+    # Now you can test output or reactive values
+    expect_true(!is.null(rv$data_display))
+  })
+
+})
+
