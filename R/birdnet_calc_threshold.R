31b5b47777ba66eecc9a6a511f00c6b38792f4b7
Sunny Tseng <sunnyyctseng@gmail.com>
2025-06-12 16:46:34 +0200

    added the comments for lines

--- R/birdnet_calc_threshold.R
+++ R/birdnet_calc_threshold.R
@@ -107,23 +107,27 @@ birdnet_calc_threshold <- function(validated_data,
     dplyr::distinct(common_name) |>
     dplyr::pull(common_name)
 
-
+  # create a progress bar
   pb <- cli::cli_progress_bar("Calculating thresholds",
                               total = length(species_list))
 
+  # start looping through the species using purrr::map_dfr
   results <- purrr::map_dfr(species_list, function(species_ind) {
 
     cli::cli_alert_info("Processing species: {.field {species_ind}}")
 
+    # individual species data
     species_data <- validated_data |>
       birdnet_filter_species(species = species_ind)
 
+    # individual species model
     species_model <- glm(
       formula = validation ~ confidence,
       family = binomial,
       data = species_data
     )
 
+    # find threshold based on the precision
     if (method == "precision") {
       species_data_full <- full_data |>
         birdnet_filter_species(species = species_ind)
@@ -139,14 +143,14 @@ birdnet_calc_threshold <- function(validated_data,
       t_target <- precision2threshold(threshold_table = threshold_table,
                                       precision_target = precision)
 
-
+    # find threshold based on probability
     } else if (method == "probability") {
 
       t_target <- (log(probability / (1 - probability)) - coef(species_model)[1]) / coef(species_model)[2]
 
     }
 
-
+    # clamp the threshold to [0, 1] range
     if (t_target < min(validated_data$confidence, na.rm = TRUE) || t_target > 1) {
 
       t_target <- max(min(validated_data$confidence, na.rm = TRUE),
@@ -157,12 +161,15 @@ birdnet_calc_threshold <- function(validated_data,
       )
     }
 
+    # combine the results into one single dataframe
     dplyr::tibble(
       common_name = species_ind,
       threshold = t_target
     )
   })
 
+
+  # final output message to remind the set value
   if (method == "precision") {
     cli::cli_alert_success("Thresholds calculated to achieve {.val {precision}} precision.")
   } else {
@@ -171,83 +178,3 @@ birdnet_calc_threshold <- function(validated_data,
 
   return(results)
 }
-
-  # # start the for loop across the species
-  # thresholds <- numeric(length(species_list))
-  # names(thresholds) <- species_list
-  #
-  # pb <- cli::cli_progress_bar("Calculating thresholds", total = length(species_list))
-  #
-  # for (species_ind in species_list) {
-  #   cli::cli_alert_info("Processing species: {.field {species_ind}}")
-  #   cli::cli_progress_update()
-  #
-  #   # get the species model ready
-  #   species_data <- validated_data |>
-  #     filter(common_name == species_ind)
-  #
-  #   species_model <- species_data |>
-  #     glm(
-  #       formula = validation ~ confidence,
-  #       family = binomial
-  #     )
-  #
-  #
-  #
-  #   if (method == "precision") {
-  #     # use the built model to covert confidence to probability
-  #     species_data_full <- full_data |>
-  #       filter(common_name == species_ind)
-  #
-  #     species_probability <- species_data_full |>
-  #       mutate(probability = predict(species_model,
-  #         newdata = species_data_full,
-  #         type = "response"
-  #       ))
-  #
-  #     # create a table of threshlds with precision
-  #     threshold_table <- tibble(threshold = seq(0, 1, 0.001)) |>
-  #       mutate(precision = map_dbl(
-  #         .x = threshold,
-  #         .f = ~ threshold2precision(species_probability, .x)
-  #       ))
-  #
-  #     # find the threshold that gives the target precision
-  #     t_target <- precision2threshold(
-  #       threshold_table = threshold_table,
-  #       precision_target = precision
-  #     )
-  #   } else if (method == "probability") {
-  #     # use the built model to convert confidence to probability (using validation dataset)
-  #     t_target <- (log(probability / (1 - probability)) - species_model$coefficients[1]) / species_model$coefficients[2]
-  #   }
-  #
-  #
-  #   if (t_target < min(validated_data$confidence, na.rm = TRUE) || t_target > 1) {
-  #     # clamp the threshold to [0, 1] range
-  #     t_target <- max(
-  #       min(validated_data$confidence, na.rm = TRUE),
-  #       min(t_target, 1)
-  #     )
-  #
-  #     cli::cli_alert_warning(
-  #       "Calculated threshold for {.val {species_ind}} is outside [0, 1] range and has been clamped."
-  #     )
-  #   }
-  #
-  #   # assign the threshold to the species
-  #   thresholds[species_ind] <- t_target
-  # }
-  #
-  # cli::cli_progress_done()
-  #
-  # if (method == "precision") {
-  #   cli::cli_alert_success("Thresholds calculated to achieve at least {.val {precision}} precision.")
-  # } else if (method == "probability") {
-  #   cli::cli_alert_success("Thresholds calculated to correspond to {.val {probability}} predicted probability.")
-  # }
-  #
-  # return(tibble(
-  #   common_name = names(thresholds),
-  #   threshold = thresholds
-  # ))
