621a140f4f53a0e98fa3797bb70919afd12263d3
Sunny Tseng <sunnyyctseng@gmail.com>
2025-06-16 16:20:32 +0200

    fixed the tibble dependency

--- tests/testthat/test-birdnet_calc_threshold.R
+++ tests/testthat/test-birdnet_calc_threshold.R
@@ -1,5 +1,5 @@
 test_that("Returns correct structure for precision method", {
-  validated <- tibble::tibble(
+  validated <- dplyr::tibble(
     common_name = rep("Species A", 10),
     confidence = seq(0.1, 1, length.out = 10),
     validation = c(0, 0, 0, 0, 0, 1, 1, 1, 0, 1)
@@ -20,7 +20,7 @@ test_that("Returns correct structure for precision method", {
 
 
 test_that("Returns correct structure for probability method", {
-  validated <- tibble::tibble(
+  validated <- dplyr::tibble(
     common_name = rep("Species A", 10),
     confidence = seq(0.1, 1, length.out = 10),
     validation = c(0, 0, 0, 0, 0, 1, 1, 1, 0, 1)
@@ -40,7 +40,7 @@ test_that("Returns correct structure for probability method", {
 
 
 test_that("Method is overridden by non-null precision/probability", {
-  validated <- tibble::tibble(
+  validated <- dplyr::tibble(
     common_name = rep("Species A", 10),
     confidence = seq(0.1, 1, length.out = 10),
     validation = c(0, 0, 0, 0, 0, 1, 1, 1, 0, 1)
@@ -60,7 +60,7 @@ test_that("Method is overridden by non-null precision/probability", {
 
 
 test_that("Throws error when both precision and probability are given", {
-  validated <- tibble::tibble(
+  validated <- dplyr::tibble(
     common_name = rep("Species D", 10),
     confidence = seq(0.1, 1, length.out = 10),
     validation = sample(0:1, 10, replace = TRUE)
@@ -79,7 +79,7 @@ test_that("Throws error when both precision and probability are given", {
 
 
 test_that("Throws error when validation values are invalid", {
-  invalid_validated <- tibble::tibble(
+  invalid_validated <- dplyr::tibble(
     common_name = rep("Species E", 10),
     confidence = seq(0.1, 1, length.out = 10),
     validation = rep(2, 10)  # Invalid
@@ -97,7 +97,7 @@ test_that("Throws error when validation values are invalid", {
 
 
 test_that("Throws error when required columns are missing", {
-  bad_df <- tibble::tibble(
+  bad_df <- dplyr::tibble(
     species = rep("Species F", 10),
     confidence = runif(10),
     validation = sample(0:1, 10, replace = TRUE)
@@ -115,7 +115,7 @@ test_that("Throws error when required columns are missing", {
 
 
 test_that("Falls back to validated_data if full_data is NULL", {
-  validated <- tibble::tibble(
+  validated <- dplyr::tibble(
     common_name = rep("Species A", 10),
     confidence = seq(0.1, 1, length.out = 10),
     validation = c(0, 0, 0, 0, 0, 1, 1, 1, 0, 1)
@@ -133,7 +133,7 @@ test_that("Falls back to validated_data if full_data is NULL", {
 
 
 test_that("Clamps thresholds outside observed range", {
-  validated <- tibble::tibble(
+  validated <- dplyr::tibble(
     common_name = rep("Species A", 10),
     confidence = seq(0.9, 1, length.out = 10),
     validation = c(0, 0, 0, 1, 1, 1, 1, 1, 0, 1)
