1293158609a0edea1ab1ee1073b5d063d496e697
Sunny Tseng <sunnyyctseng@gmail.com>
2026-06-15 17:05:21 -0700

    reduced the species number in the example dataset

--- data-raw/example_jprf_2023.R
+++ data-raw/example_jprf_2023.R
@@ -2,7 +2,20 @@
 
 library(tidyverse)
 library(here)
+library(birdnetTools)
 
-example_jprf_2023 <- read_csv(here("data-raw", "example_jprf_2023.csv"))
+example_jprf_2023 <- read_csv(here("data-raw", "example_jprf_2023.csv")) %>%
+  birdnet_filter(species = c("Swainson's Thrush",
+                             "American Robin",
+                             "Pacific-slope Flycatcher",
+                             "Pacific Wren",
+                             "Varied Thrush",
+                             "American Crow",
+                             "Yellow-rumped Warbler",
+                             "White-throated Sparrow",
+                             "Olive-sided Flycatcher",
+                             "Wilson's Warbler",
+                             "Orange-crowned Warbler",
+                             "Red-breasted Nuthatch"))
 
 usethis::use_data(example_jprf_2023, overwrite = TRUE)
